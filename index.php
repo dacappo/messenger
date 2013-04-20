3106791bddcac7a6d6dbd97eca732666e498f4e9
dacappo <patrickspiegel@web.de>
2013-04-20 14:00:35 +0200

    Raw page structure added

--- index.php
+++ index.php
@@ -2,10 +2,16 @@
 <html>
 
 <head>
+
+	<!-- Meta information -->
 	<meta content="text/html; charset=utf-8" http-equiv="Content-Type" />
 	<title>Messenger</title>
 	
+	<!-- Scripts & libraries -->
 	<script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+	
+	<!-- Cascading style sheets -->
+	<link rel="stylesheet" type="text/css" href="stylesheets/overall.css">
 	<link rel="stylesheet" type="text/css" media="screen and (min-width: 25cm)" href="stylesheets/default.css">
 	<link rel="stylesheet" type="text/css" media="screen and (min-width: 10cm) and (max-width: 25cm)" href="stylesheets/tablet.css">
 	<link rel="stylesheet" type="text/css" media="screen and (max-width: 10cm)" href="stylesheets/smartphone.css">
@@ -13,7 +19,12 @@
 </head>
 
 <body>
-	
+	<div id="action_bar">
+	</div>
+	<div id="contact_list">
+	</div>
+	<div id="conversation">
+	</div>	
 </body>
 
 </html>
