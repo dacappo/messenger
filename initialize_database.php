e4fb1b1e0868c19405864fe71d1bf6586ae0424d
dacappo <patrickspiegel@web.de>
2013-05-13 13:10:54 +0200

    db init update

--- initialize_database.php
+++ initialize_database.php
@@ -1,24 +1,23 @@
 <?php
 //$url = parse_url(getenv("mysql://bed9db9ba17777:5da87c13@us-cdbr-east-03.cleardb.com/heroku_fe4264edeb6329e?reconnect=true"));
-//$server = $url["host"];
-//$username = $url["user"];
-//$password = $url["pass"];
-//$db = substr($url["path"],1);
 
-$con = mysql_connect("us-cdbr-east-03.cleardb.com", "bed9db9ba17777", "5da87c13", "heroku_fe4264edeb6329eb");
-// Check connection
-if (mysqli_connect_errno()){
-    echo "Failed to connect to MySQL: " . mysqli_connect_error();
+
+$db = new mysqli('us-cdbr-east-03.cleardb.com', 'bed9db9ba17777', '5da87c13', 'heroku_fe4264edeb6329eb');
+
+if($db->connect_errno > 0){
+    die('Unable to connect to database [' . $db->connect_error . ']');
 }
 
 // Create table
 $sql="CREATE TABLE users(id int NOT NULL PRIMARY KEY, mobileNumber int, password CHAR(30))";
 
-// Execute query
-if (mysqli_query($con,$sql)){
-    echo "Table users created successfully";
-}else{
-    echo "Error creating table: " . mysqli_error($con);
+
+$sql = <<<SQL
+    CREATE TABLE users(id int NOT NULL PRIMARY KEY, mobileNumber int, password CHAR(30))
+SQL;
+
+if(!$result = $db->query($sql)){
+    die('There was an error running the query [' . $db->error . ']');
 }
 
 ?>
