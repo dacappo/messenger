0170144fac2bd0ee009f361e5a18e17c627f0d5d
dacappo <patrickspiegel@web.de>
2013-05-13 15:31:42 +0200

    db update

--- initialize_database.php
+++ initialize_database.php
@@ -14,14 +14,6 @@ mysql_select_db($db);
 echo("success");
 
 
-//$url = parse_url(getenv("mysql://bed9db9ba17777:5da87c13@us-cdbr-east-03.cleardb.com/heroku_fe4264edeb6329e?reconnect=true"));
-/*
-$db = new mysqli('us-cdbr-east-03.cleardb.com', 'bed9db9ba17777', '5da87c13', 'heroku_fe4264edeb6329e');
-
-if($db->connect_errno > 0){
-    die('Unable to connect to database [' . $db->connect_error . ']');
-}
-
 // Create table
 $sql="CREATE TABLE users(id int NOT NULL PRIMARY KEY, mobileNumber int, password CHAR(30))";
 
