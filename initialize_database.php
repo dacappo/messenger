05c582da5fd665aef67f72fb7b7be38a991c4c26
dacappo <patrickspiegel@web.de>
2013-05-13 12:47:05 +0200

    database ini

--- initialize_database.php
+++ initialize_database.php
@@ -5,11 +5,24 @@
 //$password = $url["pass"];
 //$db = substr($url["path"],1);
 
-mysql_connect("us-cdbr-east-03.cleardb.com", "bed9db9ba17777", "5da87c13");
+$con = mysql_connect("us-cdbr-east-03.cleardb.com", "bed9db9ba17777", "5da87c13", "heroku_fe4264edeb6329eb");
+// Check connection
+if (mysqli_connect_errno())
+{
+    echo "Failed to connect to MySQL: " . mysqli_connect_error();
+}
 
+// Create table
+$sql="CREATE TABLE users(id int NOT NULL PRIMARY KEY, number int, password CHAR(30))";
 
-mysql_select_db("heroku_fe4264edeb6329eb");
-
-
+// Execute query
+if (mysqli_query($con,$sql))
+{
+    echo "Table users created successfully";
+}
+else
+{
+    echo "Error creating table: " . mysqli_error($con);
+}
 
 ?>
