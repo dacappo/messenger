a60adc18b1568923a26c2a9877f02ab1fbc8a853
dacappo <patrickspiegel@web.de>
2013-05-13 12:56:52 +0200

    update db init

--- initialize_database.php
+++ initialize_database.php
@@ -7,21 +7,17 @@
 
 $con = mysql_connect("us-cdbr-east-03.cleardb.com", "bed9db9ba17777", "5da87c13", "heroku_fe4264edeb6329eb");
 // Check connection
-if (mysqli_connect_errno())
-{
+if (mysqli_connect_errno()){
     echo "Failed to connect to MySQL: " . mysqli_connect_error();
 }
 
 // Create table
-$sql="CREATE TABLE users(id int NOT NULL PRIMARY KEY, number int, password CHAR(30))";
+$sql="CREATE TABLE users(id int NOT NULL PRIMARY KEY, mobileNumber int, password CHAR(30))";
 
 // Execute query
-if (mysqli_query($con,$sql))
-{
+if (mysqli_query($con,$sql)){
     echo "Table users created successfully";
-}
-else
-{
+}else{
     echo "Error creating table: " . mysqli_error($con);
 }
 
