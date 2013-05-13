0782e0399ceb4bc293f822315180dbebee4cb0dd
dacappo <patrickspiegel@web.de>
2013-05-13 20:50:37 +0200

    test

--- initialize_database.php
+++ initialize_database.php
@@ -1,5 +1,6 @@
 <?php
-$url=parse_url(getenv("mysql://bed9db9ba17777:5da87c13@us-cdbr-east-03.cleardb.com/heroku_fe4264edeb6329e?reconnect=true"));
+// mysql://bed9db9ba17777:5da87c13@us-cdbr-east-03.cleardb.com/heroku_fe4264edeb6329e?reconnect=true
+$url=parse_url(getenv("CLEARDB_DATABASE_URL"));
 
 $server = $url["host"];
 $username = $url["user"];
