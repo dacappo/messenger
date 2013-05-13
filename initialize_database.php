cf55d7e846b6ae690b72c05ac847ddde9ded232c
dacappo <patrickspiegel@web.de>
2013-05-13 15:33:22 +0200

    a

--- initialize_database.php
+++ initialize_database.php
@@ -13,11 +13,6 @@ mysql_select_db($db);
 
 echo("success");
 
-
-// Create table
-$sql="CREATE TABLE users(id int NOT NULL PRIMARY KEY, mobileNumber int, password CHAR(30))";
-
-
 $sql = <<<SQL
     CREATE TABLE users(id int NOT NULL PRIMARY KEY, mobileNumber int, password CHAR(30))
 SQL;
@@ -25,7 +20,6 @@ SQL;
 if(!$result = $db->query($sql)){
     die('There was an error running the query [' . $db->error . ']');
 }
-
 mysqli_close($con);
-*/
+
 ?>
