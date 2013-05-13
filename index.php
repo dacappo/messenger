4adbd63abb24ba5a9bb0891139084cd331782211
dacappo <patrickspiegel@web.de>
2013-05-13 11:09:40 +0200

    Logout function and session handling added

--- index.php
+++ index.php
@@ -1,3 +1,8 @@
+
+<?php 
+
+
+?>
 <!DOCTYPE html>
 <html>
 
@@ -68,6 +73,39 @@
         	return false;
     	});
 	});
+	
+<?php
+    session_start();
+	if (isset($_SESSION['loggedIn']) && $_SESSION['loggedIn']==true) {
+	
+		echo   'var message = document.createElement("div");
+ 				message.innerHTML = "Succesfully logged in!";
+ 				message.setAttribute("class","info_message_success");
+ 				message.setAttribute("id","login_info_message");
+ 				document.getElementById("form_login").innerHTML = "";
+ 				document.getElementById("form_login").appendChild(message);
+ 				var button = document.createElement("div");
+ 				button.innerHTML = "Logout";
+ 				button.setAttribute("class","button");
+ 				button.setAttribute("id","button_logout");
+ 				button.onclick = function(){
+ 				    $.ajax({url: "logout.php"});
+ 				    document.getElementById("action_bar").removeChild(document.getElementById("button_logout"));
+ 				    var message = document.createElement("div");
+ 				    message.innerHTML = "Succesfully logged out!";
+ 				    message.setAttribute("class","info_message_success");
+ 				    message.setAttribute("id","login_info_message");
+ 				    document.getElementById("form_login").innerHTML = "";
+ 				    document.getElementById("form_login").appendChild(message);
+
+ 				};
+
+ 				document.getElementById("action_bar").appendChild(button);
+  				';
+	}
+
+?>
+
 </script>
 	
 </body>
--- login.php
+++ login.php
@@ -5,20 +5,39 @@
 	$success = false;
 	
 	if ($number == "84d89877f0d4041efb6bf91a16f0248f2fd573e6af05c19f96bedb9f882f7882" && $password == "050f993ea2322d4b6940f8560a253a11709fdc5ab08fd994bceb096846ea1645") {
+		session_start();		
+		$_SESSION['loggedIn']=true;
 		$success = true;
 	}
 
-
 	header ("content-type: text/javascript");
+	
 	if($success) {
+				
  		echo 	'
- 		
- 				var message = document.createElement("div");
+ 		        var message = document.createElement("div");
  				message.innerHTML = "Succesfully logged in!";
  				message.setAttribute("class","info_message_success");
  				message.setAttribute("id","login_info_message");
  				document.getElementById("form_login").innerHTML = "";
  				document.getElementById("form_login").appendChild(message);
+ 				var button = document.createElement("div");
+ 				button.innerHTML = "Logout";
+ 				button.setAttribute("class","button");
+ 				button.setAttribute("id","button_logout");
+ 				button.onclick = function(){
+ 				    $.ajax({url: "logout.php"});
+ 				    document.getElementById("action_bar").removeChild(document.getElementById("button_logout"));
+ 				    var message = document.createElement("div");
+ 				    message.innerHTML = "Succesfully logged out!";
+ 				    message.setAttribute("class","info_message_success");
+ 				    message.setAttribute("id","login_info_message");
+ 				    document.getElementById("form_login").innerHTML = "";
+ 				    document.getElementById("form_login").appendChild(message);
+
+ 				};
+
+ 				document.getElementById("action_bar").appendChild(button);
   				';
  	} else {
  		echo 	'
