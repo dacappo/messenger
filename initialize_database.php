d9d8f6c90cb3e22c7709f1086c09b2d86b411a76
dacappo <patrickspiegel@web.de>
2013-05-13 17:03:48 +0200

    test

--- initialize_database.php
+++ initialize_database.php
@@ -19,7 +19,7 @@ SQL;
 */
 if(!$result = $db->query($sql)){
     die('There was an error running the query [' . $db->error . ']');
-} 
+}
 /*
 if(!$result = $db->query($sql2)){
     die('There was an error running the query [' . $db->error . ']');
