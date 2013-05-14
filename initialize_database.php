dd8a34c3ad82ca120fd7adfe504a5b143170a669
dacappo <patrickspiegel@web.de>
2013-05-14 10:04:50 +0200

    test

--- initialize_database.php
+++ initialize_database.php
@@ -21,14 +21,12 @@ $selected = mysql_select_db($db,$connection)
     or die("Could not select examples");
 echo("Database selected!<br>");
 
-
-$sql = 'CREATE TABLE users(id int NOT NULL PRIMARY KEY, mobileNumber CHAR(66), password CHAR(66))';
-
-if(!$result = $db->query($sql)){
-    die('There was an error running the query [' . $db->error . ']');
-}
+$result = mysql_query("CREATE TABLE users(id int NOT NULL PRIMARY KEY, mobileNumber CHAR(66), password CHAR(66))")
+    or die("There was an error running the query !<br>");
+echo("Table created!<br>");
 
 
-mysqli_close($con);
+mysqli_close($connection);
+echo("Connection closed!");
 
 ?>
