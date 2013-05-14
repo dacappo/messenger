daf051dfced6a9985e887cfdef0c1682016337e0
dacappo <patrickspiegel@web.de>
2013-05-14 10:08:54 +0200

    test

--- initialize_database.php
+++ initialize_database.php
@@ -21,6 +21,10 @@ $selected = mysql_select_db($db,$connection)
     or die("Could not select examples");
 echo("Database selected!<br>");
 
+$result = mysql_query("DROP TABLE users")
+    or die("There was an error running the query !<br>");
+echo("Table dropped!<br>");
+
 $result = mysql_query("CREATE TABLE users(id int NOT NULL PRIMARY KEY, mobileNumber CHAR(66), password CHAR(66))")
     or die("There was an error running the query !<br>");
 echo("Table created!<br>");
