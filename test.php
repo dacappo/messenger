59342e81730435d260f29c2799b1d1cc86ed750a
dacappo <patrickspiegel@web.de>
2013-05-15 11:50:43 +0200

    test

--- test.php
+++ test.php
@@ -2,7 +2,7 @@
 include "check_login.php";
 
 if (check_database('84d89877f0d4041efb6bf91a16f0248f2fd573e6af05c19f96bedb9f882f7882','050f993ea2322d4b6940f8560a253a11709fdc5ab08fd994bceb096846ea1645')) {
-    echo("test successfull");
-} else if (!check_database('84d89877f0d4041efb6bf91a16f0248f2fd573e6af05c19f96bedb9f882f7882','050f993ea2322d4b6940f8560a253a11709fdc5ab08fd994bceb096846ea1645'){
-    echo("test not successfull");
+    echo("check_database: <span style='color:green'>successful</span><br>");
+} else {
+    echo("check_database: <span style='color:red'>not successful</span><br>");
 }
