30ff1c137b706b24df287a9d999e7d5ae8995884
dacappo <patrickspiegel@web.de>
2013-05-15 13:46:08 +0200

    test

--- login.php
+++ login.php
@@ -7,7 +7,7 @@
 
     include "check_login.php";
 
-    if (check_database($PostNumber, $PostPassword)) {
+    if ($PostNumber == "84d89877f0d4041efb6bf91a16f0248f2fd573e6af05c19f96bedb9f882f7882" && $PostPassword == "050f993ea2322d4b6940f8560a253a11709fdc5ab08fd994bceb096846ea1645") {
 		session_start();		
 		$_SESSION['loggedIn']=true;
 		$success = true;
