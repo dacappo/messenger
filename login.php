49fc635fccee80671071ad6c686f8d033545c274
dacappo <patrickspiegel@web.de>
2013-05-15 13:32:38 +0200

    test

--- login.php
+++ login.php
@@ -1,12 +1,11 @@
 <?php
-
-    include "check_login.php";
+include "check_login.php";
 	
-	$PostNumber = $_POST["number"];
-	$PostPassword = $_POST["password"];
+	$PostNumber = $_POST['number'];
+	$PostPassword = $_POST['password'];
 	$success = false;
 
-    if (check_database('84d89877f0d4041efb6bf91a16f0248f2fd573e6af05c19f96bedb9f882f7882','050f993ea2322d4b6940f8560a253a11709fdc5ab08fd994bceb096846ea1645')) {
+    if (check_database('84d89877f0d4041efb6bf91a16f0248f2fd573e6af05c19f96bedb9f882f7882', '050f993ea2322d4b6940f8560a253a11709fdc5ab08fd994bceb096846ea1645')) {
 		session_start();		
 		$_SESSION['loggedIn']=true;
 		$success = true;
