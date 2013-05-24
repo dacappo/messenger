ecee456aec51bda7d66c9d96d466ff2dbc378ad3
Lukas Carullo <lukas.carullo@gmail.com>
2013-05-24 10:33:21 +0200

    new function organisation for register

--- test.php
+++ test.php
@@ -1,6 +1,6 @@
 <?php
 include "check_login.php";
-include "registration_services.php";
+include "registration.php";
 
 if (check_database('84d89877f0d4041efb6bf91a16f0248f2fd573e6af05c19f96bedb9f882f7882','050f993ea2322d4b6940f8560a253a11709fdc5ab08fd994bceb096846ea1645')) {
     echo("check_database(): correct input = <span style='color:green'>successful</span><br>");
