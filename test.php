1bfe76675e63a9b3f980df06f5670c2631dc2098
Lukas Carullo <lukas.carullo@gmail.com>
2013-05-24 11:00:58 +0200

    table formation of test.php

--- test.php
+++ test.php
@@ -6,11 +6,12 @@ include "registration.php";
     <thead>
     <tr>
         <td>TestCase</td>
+        <td>Checked path</td>
         <td>Status</td>
     </tr>
     </thead>
     <tr>
-        <td>Test valid user login</td>
+        <td>Tes user login</td>
         <td><?php
             if (checkDatabaseForUser('84d89877f0d4041efb6bf91a16f0248f2fd573e6af05c19f96bedb9f882f7882', '050f993ea2322d4b6940f8560a253a11709fdc5ab08fd994bceb096846ea1645')) {
                 echo("checkDatabaseForUser(): correct input </td><td> <span style='color:green'>successful</span><br></td>");
@@ -20,7 +21,7 @@ include "registration.php";
             ?></td>
     </tr>
     <tr>
-        <td>Test incorrect user login</td>
+        <td>Test user login</td>
         <td><?php
             if (checkDatabaseForUser(' ', ' ') == false) {
                 echo("checkDatabaseForUser(): wrong input </td><td> <span style='color:green'>successful</span><br></td>");
@@ -35,8 +36,8 @@ include "registration.php";
             /* Check HTTP request to GATEWAY provider for registration */
             $returnMessage = register("015140445799", generateValidationString(5));
             echo (!strncmp($returnMessage, "OK", 2)) ?
-                "register(): connection to SMS gateway provider = <span style='color:green'>successful $returnMessage</span><br>" :
-                "register(): Error => <span style='color:red'>$returnMessage</span><br>";
+                "register(): connection to SMS gateway provider  </td><td>  <span style='color:green'>successful</span><br></td>" :
+                "register(): Error  </td><td>  <span style='color:red'>$returnMessage</span><br></td>";
             ?></td>
     </tr>
 </table>
