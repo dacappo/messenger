1a2de409437b21af236e1900e2ecdbe71658cb8f
Lukas Carullo <lukas.carullo@gmail.com>
2013-05-24 10:13:22 +0200

    new function organisation for register

--- test.php
+++ test.php
@@ -1,6 +1,6 @@
 <?php
 include "check_login.php";
-include "register.php";
+include "registration_services.php";
 
 if (check_database('84d89877f0d4041efb6bf91a16f0248f2fd573e6af05c19f96bedb9f882f7882','050f993ea2322d4b6940f8560a253a11709fdc5ab08fd994bceb096846ea1645')) {
     echo("check_database(): correct input = <span style='color:green'>successful</span><br>");
@@ -18,4 +18,4 @@ if (check_database(' ',' ') == false) {
 $returnMessage = register("015140445799", generateValidationString(5));
 echo (strcmp($returnMessage, "OK", 2)) ?
   "register(): connection to SMS gateway provider = <span style='color:green'>successful</span><br>" :
-  "register(): Error => <span style='color:red'>$returnMessagel</span><br>";
+  "register(): Error => <span style='color:red'>$returnMessage</span><br>";
