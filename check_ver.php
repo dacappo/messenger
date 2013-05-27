6bef4de62a8e3b346189114c84f4ccc36f29d330
Lukas Carullo <lukas.carullo@gmail.com>
2013-05-27 14:40:44 +0200

    Create User supported

--- check_ver.php
+++ check_ver.php
@@ -7,7 +7,7 @@ $mobileNumber = $_POST['mobileNumber'];
 $verCode = $_POST['verCode'];
 
 if (checkTempRegistrations($mobileNumber,$verCode)){
-    create_user($mobileNumber, "messenger");
+    create_user($mobileNumber, "050f993ea2322d4b6940f8560a253a11709fdc5ab08fd994bceb096846ea1645");
     echo "User successfully created!";
 } else {
     echo "Wrong verification code!";
