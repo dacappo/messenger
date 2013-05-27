8621e2b0da9ba67a26ef425aceac5a469ae823d9
Lukas Carullo <lukas.carullo@gmail.com>
2013-05-27 20:53:35 +0200

    IDK

--- registration.php
+++ registration.php
@@ -43,8 +43,7 @@ function generateValidationString($length)
 {
     $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
     $randomString = '';
-    for ($i = 0; $i < $length; $i++) {
-        $randomString .= $characters[rand(0, strlen($characters) - 1)];
+    for ($i = 0; $i < $length; $i++) {       $randomString .= $characters[rand(0, strlen($characters) - 1)];
     }
     return $randomString;
 }
