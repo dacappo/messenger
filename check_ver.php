6aab10f9a2597df63d2a9d1a2816ff28dcae899b
azZur0 <lukas.carullo@gmail.com>
2013-05-27 17:02:51 +0200

    Initializiation extended and Creating user outsourced

--- check_ver.php
+++ check_ver.php
@@ -7,8 +7,7 @@ $mobileNumber = $_POST['mobileNumber'];
 $verCode = $_POST['verCode'];
 
 if (checkTempRegistrations($mobileNumber,$verCode)){
-    create_user($mobileNumber, "050f993ea2322d4b6940f8560a253a11709fdc5ab08fd994bceb096846ea1645");
-    echo "User successfully created!";
+    echo "OK : authentification successful!";
 } else {
     echo "Wrong verification code!";
 }
---
+++ create_user.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: Lukas
+ * Date: 27.05.13
+ * Time: 16:22
+*/
+include "dbconnection.php";
+include "registration.php";
+
+$mobileNumber = $_POST['mobileNumber'];
+$password = $_POST['password'];
+
+if (create_user($mobileNumber, $password)){
+    echo "OK : User created";
+} else {
+    echo "Server Error during Runtime with following parameters:" . var_dump($mobileNumber,$mobileNumber);
+}
