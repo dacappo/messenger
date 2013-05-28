f1e0b2e6da159fc1c20c94af3506a403c0739994
azZur0 <lukas.carullo@gmail.com>
2013-05-28 16:14:47 +0200

    initialize Database modified

--- initialize_database.php
+++ initialize_database.php
@@ -17,16 +17,16 @@ or die("There was an error during configuration !<br>");
 /*
  * Clear existing Tables
 */
-
+$result = mysql_query("DROP TABLE contacts")
+or die("There was an error running the query !<br>");
+echo("Table dropped!<br>");
 $result = mysql_query("DROP TABLE users")
 or die("There was an error running the query !<br>");
 echo("Table dropped!<br>");
 $result = mysql_query("DROP TABLE temp_registrations")
 or die("There was an error running the query !<br>");
 echo("Table dropped!<br>");
-$result = mysql_query("DROP TABLE contacts")
-or die("There was an error running the query !<br>");
-echo("Table dropped!<br>");
+
 
 
 /*
