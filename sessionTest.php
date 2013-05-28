6bdcad3b1b277baf388d84cf383d1d4c3a6a7b3a
Lukas Carullo <lukas.carullo@gmail.com>
2013-05-28 09:56:39 +0200

    Sessiontest

--- sessionTest.php
+++ sessionTest.php
@@ -5,12 +5,12 @@
  * Date: 28.05.13
  * Time: 08:50
  * To change this template use File | Settings | File Templates.
-*/
+ */
 
 session_start();
 
 // Create a new session object if its a new session
 if (!isset($_COOKIE['session'])) {
     setcookie("session", $_POST['test'], time() + 31536000);
-    echo $_COOKIE['session'];
+    echo $_COOKIE['session'] . $_POST['username'];
 }
