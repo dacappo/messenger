67194b80f4c5b00e3938cf3cd9a17316798483e4
azZur0 <lukas.carullo@gmail.com>
2013-06-04 21:37:14 +0200

    New test users

--- initialize_database.php
+++ initialize_database.php
@@ -62,7 +62,7 @@ echo("Table contacts created!<br>");
 $result = mysql_query("INSERT INTO users (id,mobileNumber,password) VALUES ('1','84d89877f0d4041efb6bf91a16f0248f2fd573e6af05c19f96bedb9f882f7882','050f993ea2322d4b6940f8560a253a11709fdc5ab08fd994bceb096846ea1645')")
 or die("There was an error running the query !<br>");
 echo("Example data ceated!<br>");
-$result = mysql_query("INSERT INTO users (id,mobileNumber,password) VALUES ('2','0123456789','messenger'),('3','01514044001','messenger'),('4','0160987123','messenger'),('5','01418912302','messenger')")
+$result = mysql_query("INSERT INTO users (id,mobileNumber,password) VALUES ('2','0123456789','messenger'),('3','01514044001','messenger'),('4','0160987123','messenger'),('5','01418912302','messenger'),('20','015122233344','unsicher'),('21','017511166633','unsicher'),('22','0180664433','unsicher')")
 or die("There was an error running the query !<br>");
 echo("Example data ceated!<br>");
 
