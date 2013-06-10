edbd09d966641e64822d2c6427ae19ed8132bcff
Lukas Carullo <lukas.carullo@gmail.com>
2013-06-10 15:27:51 +0200

    compare contacts

--- compare_contacts.php
+++ compare_contacts.php
@@ -4,7 +4,7 @@
  * User: Lukas
  * Date: 03.06.13
  * Time: 14:11
-*/
+ */
 
 include "messenger.php";
 include "dbMessenger.php";
@@ -12,33 +12,36 @@ include "dbMessenger.php";
 $contacts = $_POST['contacts'];
 $user_id = $_POST['id'];
 
-if (!isset($contacts) && !isset($user_id)){
+if (!isset($contacts) && !isset($user_id)) {
     echo "Not all required POST parameters are set";
 }
 
-if (strlen($contacts) < 4){
+if (strlen($contacts) < 4) {
     echo "Invalid JSON : Too short!";
 }
 
 $arrayOfContacts = json_decode($contacts, true);
 
-if (isset($arrayOfContacts)){
-   $matchedContacts = compare_contacts($arrayOfContacts, $user_id);
+if (isset($arrayOfContacts)) {
+    $matchedContacts = compare_contacts($arrayOfContacts, $user_id);
 } else {
-    echo "Server Error : during JSON decoding" . json_last_error() . var_dump($arrayOfContacts) . var_dump($contacts);
+    echo "Server Error : during JSON decoding  with JSON Error: " . json_last_error() . var_dump($arrayOfContacts) . var_dump($contacts);
 }
 
-if (!empty($matchedContacts)){
+$newCreated = false;
+$contactsExisting = false;
+if (!empty($matchedContacts)) {
     $newCreated = create_contacts($user_id, $matchedContacts);
     $contactsExisting = checkforExistingContacts($matchedContacts);
+
+    header('Content-Type: application/json');
+
+    if ($newCreated || $contactsExisting) {
+        echo createJSONResponseForNewContacts($matchedContacts, $user_id);
+    } else {
+        echo "OK : No new contacts created" . var_dump($matchedContacts);
+    }
+    
 } else {
     echo "Non of your contacts is using this messenger";
-}
-
-header('Content-Type: application/json');
-
-if ($newCreated || $contactsExisting){
-    echo createJSONResponseForNewContacts($matchedContacts, $user_id);
-} else {
-    echo "OK : No new contacts created" . var_dump($matchedContacts) . var_dump($newCreated).var_dump($allExisting);
 }
