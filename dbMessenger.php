8e3f79c9e0873f0eb4f7f2107b75e816578de4c5
azZur0 <lukas.carullo@gmail.com>
2013-06-16 10:22:57 +0200

    $all fuctionality in receive message

--- dbMessenger.php
+++ dbMessenger.php
@@ -214,7 +214,7 @@ function getMessagesFromDB($contact_id, $opposite_contact_id, $all)
         $query = 'SELECT * FROM messages WHERE contact_id=' . $contact_id . ' OR contact_id=' . $opposite_contact_id . ' ORDER BY date_time DESC LIMIT 10;';
 
     } else {
-        $query = 'SELECT * FROM messages WHERE contact_id=' . $contact_id . ' OR contact_id=' . $opposite_contact_id . ' AND read_status = 0 ORDER BY date_time DESC ;';
+        $query = 'SELECT * FROM messages WHERE (contact_id=' . $contact_id . ' OR contact_id=' . $opposite_contact_id . ') AND read_status = 0 ORDER BY date_time DESC ;';
     }
     echo var_dump($query);
     //Connect to DB
